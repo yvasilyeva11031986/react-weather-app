0fe8bc2746b54c48a93b74587c7917834091b123
yvasilyeva11031986 <y.vasilyevaHR@gmail.com>
2023-01-14 17:15:33 +0000

    Corrections
    ++

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
+// eslint-disable-next-line no-unused-vars
 import Styles from './Styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
